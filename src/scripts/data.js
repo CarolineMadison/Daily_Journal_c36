2cf96d97e22edc8089daf95a24e4e4de1052ecdb
Caroline Brownlee <brownleecaroline@gmail.com>
2019-10-29 13:55:16 -0500

    finished pt 7 of daily journal. Entry form reads input values, saves it to the database, and then prints it on the dom with all other entries

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,7 +1,18 @@
+const url = "http://localhost:8088/entries"
+
 const API = {
     getJournalEntries() {
-        return fetch("http://localhost:8088/entries")
+        return fetch(`${url}`)
             .then(response => response.json())
+    },
+    saveJournalEntry(journalEntryObject) {
+        fetch(`${url}`, { // Replace "url" with your API's URL
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(journalEntryObject)
+        })
     }
 }
 
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -7,23 +7,57 @@
 import API from "./data.js"
 import entryToDom from "./entriesDOM.js"
 import makeJournalEntryComponent from "./entryComponent.js"
-    // fetch call that gets entries array from database.json
-    API.getJournalEntries().then(parsedEntries => {
-        console.log(parsedEntries)
-        // iterates over entries array and for each entry 
-        parsedEntries.forEach(entry => {
-            // invokes makeJournalEntryComponent function and stores the returned string in a variable
-            const innerHtmlObject = makeJournalEntryComponent(
-                entry.date,
-                entry.concept,
-                entry.entry,
-                entry.mood
-            )
-            console.log(makeJournalEntryComponent)
-            // invokes function that puts each entry on the dom. Takes innerHtmlObject string as an argument
-            entryToDom(innerHtmlObject)
+// fetch call that gets entries array from database.json
+API.getJournalEntries().then(parsedEntries => {
+    // iterates over entries array and for each entry 
+    parsedEntries.forEach(entry => {
+        // invokes makeJournalEntryComponent function and stores the returned string in a variable
+        const journalHtml = makeJournalEntryComponent(
+            entry.date,
+            entry.concept,
+            entry.content,
+            entry.mood
+        )
+        // invokes function that puts each entry on the dom. Takes innerHtmlObject string as an argument
+        entryToDom(journalHtml)
+    })
+})
 
+// In your main JavaScript module (journal.js) add a click event listener to the Record Journal Entry button at the bottom of your form. When the user clicks the button, you need to create a new entry in your API. The HTTP method that you use to create resources is POST. Guidance on syntax is provided below.
+
+const date = document.querySelector("#journalDate")
+const concept = document.querySelector("#concept")
+const content = document.querySelector("#content")
+const mood = document.querySelector("#mood")
+
+const createJournalEntry = (date, concept, content, mood) => {
+    return {
+        "date": date.value,
+        "concept": concept.value,
+        "content": content.value,
+        "mood": mood.value
+    }
+}
+
+const saveEntryButton = document.querySelector("#BTN")
+saveEntryButton.addEventListener("click", () => {
+    const journalEntryObject = createJournalEntry(date, concept, content, mood)
+        API.saveJournalEntry(journalEntryObject)
+        .then(API.getJournalEntries)
+        .then(entries => {
+            const entriesContainer = document.querySelector(".entryLog")
+            entriesContainer.innerHTML = ""
+            entries.forEach(entry => {
+                const journalHTML = createJournalEntryComponent(entry)
+                entryToDom(journalHTML)
         })
     })
+            .then(() => {
+                date.value = ""
+                concept.value = ""
+                content.value = ""
+                mood.value = ""
+            })
+})
 
 
