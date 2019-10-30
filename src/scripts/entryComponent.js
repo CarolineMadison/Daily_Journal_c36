e5e58ddc1478a74606e5da5673c7307b2efc0333
Caroline Brownlee <brownleecaroline@gmail.com>
2019-10-30 16:12:36 -0500

    added delete button to each entry and applied css styling. Ready to start writing functionality.

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -1,12 +1,15 @@
-// Function that returns an html string representation for one journal entry.
+// Function that turns one entry object into an html string representation of that object
 const makeJournalEntryComponent = (date, concept, entry, mood) => {
     return `
+        <section class="journalEntryHTML--${entry.id}">
         <div class="journalEntryHTML">
             <h1>${date}</h1><hr />
             <p>CONCEPT: <br><br> ${concept}</p><br>
             <p>ENTRY: <br><br> ${entry}</p><br>
             <p>MOOD: <br>${mood}</p>
+            <button id="deleteEntry--${entry.id}">Delete Entry</button>
         </div>
+        </section>
     `
 }
 
